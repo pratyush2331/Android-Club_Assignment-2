8e261bb28f62141c6bd038f44520741b3677afe6
Pratyush Raj <72189926+pratyush2331@users.noreply.github.com>
2022-01-21 00:02:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import MainScreen from './src/screens/MainScreen';
+
+export default function App() {
+  return (
+
+    <>
+      <MainScreen />
+    </>
+
+  );
+}
