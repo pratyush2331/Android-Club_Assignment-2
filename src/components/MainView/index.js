66a0b32c4a1b43115cc682665dcf0c2ffe7cc989
Pratyush Raj <72189926+pratyush2331@users.noreply.github.com>
2022-01-21 00:35:42 +0530

    Update index.js

--- src/components/MainView/index.js
+++ src/components/MainView/index.js
@@ -6,7 +6,7 @@ import PratyushInput from '../CustomTextInput';
 export default function MainView(){
     return(
     <View style={styles.container}>
-      <Text>Welcome to CodeStar</Text>
+      <Text style={{padding:20, fontSize:30}}>Welcome to CodeStar</Text>
       <Image 
         source={{
             uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQwO9tbsu2JNlt_4PBhLa7Qhfbhwp_zimmew&usqp=CAU',
@@ -14,7 +14,7 @@ export default function MainView(){
         style={{width: 200, height: 200}}
       /> 
       <Text style={{padding:10}}>Enter Your User ID: </Text>
-      <PratyushInput placeholder="Enter Your Email ID" placeholderColor="#fff"/>
+      <PratyushInput placeholder="example@gmail.com" placeholderColor="#fff"/>
 
       <Text style={{padding:10}}>Enter Your Password: </Text>
       <TextInput secureTextEntry={true} style={{}} placeholder='Password' style={{backgroundColor: "#fff"}}></TextInput>
